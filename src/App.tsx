b4be2b6c3fd6c94295032748b8edb270d28eac57
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-06-12 11:53:04 +0200

    Implement error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import React, { Suspense } from 'react';
+import { ThemeProvider, CssBaseline, LinearProgress } from '@mui/material';
 import { Provider } from 'react-redux';
 import { theme } from './theme/theme';
 
 import { AppRoutes } from './routes';
 import { store } from './redux/store';
+import { ErrorBoundary } from './components/errors';
+import { ToastContainer } from 'react-toastify';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <CssBaseline />
     <Provider store={store}>
-      <AppRoutes />
+      <ErrorBoundary location={null}>
+        <CssBaseline />
+        <ToastContainer />
+        <Suspense fallback={<LinearProgress />}>
+          <AppRoutes />
+        </Suspense>
+      </ErrorBoundary>
     </Provider>
   </ThemeProvider>
 );
