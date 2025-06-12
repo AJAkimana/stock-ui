9adc481788e8b9f6d03ea5a6544aa80b6712ed9b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-06-12 11:52:40 +0200

    Fix routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router';
 import Layout from './components/Layout';
 import { AuthProvider } from './components/providers/AuthProvider';
 import Login from './pages/Login';
@@ -6,18 +6,20 @@ import Dashboard from './pages/Dashboard';
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="" element={<Login />} />
-      <Route
-        path="/admin"
-        element={
-          <AuthProvider>
-            <Layout />
-          </AuthProvider>
-        }
-      >
-        <Route path="/" element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route
+          path="admin"
+          element={
+            <AuthProvider>
+              <Layout />
+            </AuthProvider>
+          }
+        >
+          <Route path="" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
